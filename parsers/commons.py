5d8dfbea2899b4114cd3f5420eb67d0ce471578e
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-07-22 01:06:07 -0400

    Improve slugification

--- parsers/commons.py
+++ parsers/commons.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import yaml
+import re
 from slugify import slugify as slugify_
+
+STOPWORDS = [
+  'a',
+  'au',
+  'd',
+  'de',
+  'des',
+  'du',
+  'et',
+  'l',
+  'la',
+  'le',
+  'les',
+  'ou',
+  'pour',
+  'sur',
+  'un',
+  ]
 
 def export_yaml(data, file_path):
   with open(file_path, 'w') as file:
@@ -9,7 +28,5 @@ def export_yaml(data, file_path):
 
 def slugify(text, stopwords = False):
   if stopwords:
-    return slugify_(text, separator = '_', stopwords=[
-      'd', 'de', 'la', 'du', 'le', 'et', 'les', 'au', 'l', 'des', 'sur']
-      )
+    return slugify_(text, separator = '_', stopwords = STOPWORDS)
   return slugify_(text, separator = '_')
