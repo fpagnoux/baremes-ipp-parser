2c00ef3c7a0b3d2cc2d6e9dcb071b958dba9f222
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-08-22 21:34:58 -0400

    Update dependencies

--- setup.py
+++ setup.py
@@ -6,24 +6,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='OpenFisca-Baremes-IPP',
+    name='IPP-Baremes-Parser',
     version='0.1.0',
     author='Institut des Politiques Publiques',
     author_email='florian.pagnoux@gmail.com',
     include_package_data = True,  # Will read MANIFEST.in
-    install_requires=[
-        'OpenFisca-Core >= 23.3, < 24.0',
-        ],
-    extras_require = {
-        'api': [
-            'OpenFisca-Web-API >= 4.0.0, < 7.0',
-            ],
-        'test': [
-            'flake8 >= 3.4.0, < 3.5.0',
-            'flake8-print',
-            'nose',
-            ]
-        },
+    install_requires=[],
     packages=find_packages(),
     test_suite='nose.collector',
     )
