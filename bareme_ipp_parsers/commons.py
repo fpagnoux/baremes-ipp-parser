cccf5bd2eb257725ad0bf4a27408b40e1d2809b3
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-11-21 19:53:19 -0500

    Always put metadata at the end of the node

--- bareme_ipp_parsers/commons.py
+++ bareme_ipp_parsers/commons.py
@@ -35,14 +35,36 @@ def represent_str(dumper, data):
     return dumper.represent_scalar('tag:yaml.org,2002:str', data, style = '|')
   return dumper.represent_scalar('tag:yaml.org,2002:str', data)
 
+def represent_dict(dumper, data):
+  if data.get('metadata'):
+    metadata = data['metadata']
+    del data['metadata']
+    data['metadata'] = metadata
+  if data.get('documentation'):
+    documentation = data['documentation']
+    del data['documentation']
+    data['documentation'] = documentation
+
+  return dumper.represent_dict(data)
+
 yaml.default_flow_style = False
 yaml.representer.add_representer(type(None), represent_none)
 yaml.representer.add_representer(str, represent_str)
+yaml.representer.add_representer(dict, represent_dict)
 
 
 def export_yaml(data, file_path):
   with open(file_path, 'w') as file:
     yaml.dump(data, file)
+  with open(file_path, 'r') as file:
+    content = file.read()
+    new_content = re.sub(
+               r"'(\d{4}-\d{2}-\d{2})'",
+               r"\1",
+               content
+           )
+  with open(file_path, 'w') as file:
+    file.write(new_content)
 
 
 def slugify(text, stopwords = False):
